1b8c79ded2a52bb6d0933c5c14130eee7a7d3c1e
Kristýna Míšková <kika@anni.cz>
2026-05-20 10:24:59 +0000

    File changes

--- src/pages/SalesPresentation.jsx
+++ src/pages/SalesPresentation.jsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
 import useIdleRedirect from "@/hooks/useIdleRedirect";
 
-const BG_URL = "/images/sales_bg.jpg";
+const BG_URL = "https://media.base44.com/images/public/6a0abd7d4f23084851e1d83f/6326b2f45_image1.jpg";
 const LOGO_URL = "/images/logo-3.svg";
 
 const INITIAL_CIRCLES = [
